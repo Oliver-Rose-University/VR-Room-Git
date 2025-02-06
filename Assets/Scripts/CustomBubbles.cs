d2dffee1e4ba55de58fa5ceb52dc2ff40c666dea
Oliver-Rose-University <oliver.rose@student.norwichuni.ac.uk>
2025-02-06 12:14:06 +0000

    AAAA

---
+++ Assets/Scripts/CustomBubbles.cs
@@ -0,0 +1,22 @@
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+public class CustomBubbles : MonoBehaviour
+{
+    [Tooltip("The object that will be spawned")]
+    public GameObject prefab = null;
+    [Tooltip("The Transform where the object is spawned")]
+    public Transform spawnTransform = null;
+    public void Spawn(){
+        GameObject bubble = Instantiate(prefab, spawnTransform.position, spawnTransform.rotation);
+        bubble.transform.rotation = Random.rotation;
+        float launchSpeed = Random.Range(0.5f, 1.5f);
+        Vector3 force = spawnTransform.forward * launchSpeed;
+        bubble.GetComponent<Rigidbody>().AddForce(force);
+        bubble.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
+    
+    }
+
+
+
+}
